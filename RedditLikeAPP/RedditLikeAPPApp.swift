b02625f848d841bdc0bfb5f065016073dbbdcf5c
melenom <melenom@163.com>
2023-07-21 14:06:31 +0800

    Initial Commit

---
+++ RedditLikeAPP/RedditLikeAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedditLikeAPPApp.swift
+//  RedditLikeAPP
+//
+//  Created by melenom on 2023/7/21.
+//
+
+import SwiftUI
+
+@main
+struct RedditLikeAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
